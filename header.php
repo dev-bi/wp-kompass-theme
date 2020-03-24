672f48287698a99f551a58c68dd79a011ec42aa4
Stefan Simon <stefan.simon@lionysos.com>
2020-03-24 13:15:02 +0100

    Versionsdatum hinzugefügt. Schriftfarbe: orange

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <?php wp_head(); ?>
 </head>
 <body>
+    <?php include_once 'version/version.php'; ?>
     <h3>Die BI Datenbank durchsuchen</h3>
     <?php include_once 'bi-suche/suche_test.php'; ?>
     <hr>
---
+++ version/version.php
@@ -0,0 +1,4 @@
+<?php
+$day = "Dienstag";
+$date_time = "24. März 13:00";
+echo "<p class=\"version\">Geändert am: $day um: $date_time </p>";
